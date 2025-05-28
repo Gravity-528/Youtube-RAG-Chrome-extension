6b9184b24be39770b2a6eba4a01663e47fa82c85
Gravity-528 <btech10086.22@bitmesra.ac.in>
2025-05-28 21:14:29 +0530

    basic rag pipeline

--- server/helper.py
+++ server/helper.py
@@ -1,48 +1,62 @@
-from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
-from langchain_openai import OpenAIEmbeddings
-
-def ingestion_knowledge_base(videoId):
-    video_id= videoId
-
+def fetch_transcript(video_id, preferred_languages=["hi", "en"]):
     try:
-        transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["hi"])
-        print(transcript_list)
-        transcript = " ".join(chunk["text"] for chunk in transcript_list)
-        print(transcript)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=preferred_languages)
+        return transcript
     except TranscriptsDisabled:
-      print("No caption available for this video")
-      return
+        print("No caption available for this video")
+        return []
 
+def ingestion_knowledge_base(video_id):
+    transcript_list = fetch_transcript(video_id)
+    if not transcript_list:
+        return None
+
+    transcript_text = " ".join(chunk["text"] for chunk in transcript_list)
+    
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    chunks = splitter.create_documents([transcript])
+    chunks = splitter.create_documents([transcript_text])
 
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     vector_store = FAISS.from_documents(chunks, embeddings)
     return vector_store
 
-def Retrieval_from_vector_store(vector_store, query):
+def format_docs(retrieved_docs):
+    return "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+def build_qa_chain(vector_store):
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
-    docs = retriever.get_relevant_documents(query)
-    return docs
 
+    prompt_template = """You are a helpful assistant. Use the following context to answer the question.
+{context}
 
-def augmentation_prompt(query, docs):
-    prompt_template = """You are a helpful assistant. Use the following pieces of context to answer the question at the end.
-    {context}
-    Question: {question}
-    Answer:"""
+Question: {question}
+Answer:"""
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     
-    prompt = PromptTemplate(
-        template=prompt_template,
-        input_variables=["context", "question"]
-    )
-    
-    context = "\n".join([doc.page_content for doc in docs])
-    return prompt.format(context=context, question=query)
+    parallel_chain = RunnableParallel({
+        'context': retriever | RunnableLambda(format_docs),
+        'question': RunnablePassthrough()
+    })
 
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    parser = StrOutputParser()
 
+    main_chain = parallel_chain | prompt | llm | parser
+    return main_chain
 
+def ask_question(video_id, query):
+    vector_store = ingestion_knowledge_base(video_id)
+    if not vector_store:
+        return "No vector store available."
+
+    chain = build_qa_chain(vector_store)
+    answer = chain.invoke(query)
+    return answer
