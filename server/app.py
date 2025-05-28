f858eba3aaee5991a4154a25bd7976774cf89c50
Gravity-528 <btech10086.22@bitmesra.ac.in>
2025-05-28 19:30:43 +0530

    chunking youtube transcript and storing in vector_store as embeddings

--- server/app.py
+++ server/app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-# @app.route('/videoId', methods=['POST'])
-# def receive_video_id():
-#     data = request.json
-#     video_id = data.get('videoId')
-#     if video_id:
-#         print("Received videoId:", video_id)
-#         return jsonify({"status": "success", "videoId": video_id})
-#     return jsonify({"status": "error", "message": "No videoId provided"}), 400
-
 @app.route('/')
 def home():
     return "Welcome to Flask Backend!"
 
+@app.route('/rag-pipeline', methods=['POST'])
+def rag_pipeline():
+    data = request.json
+    if not data or 'videoId' not in data:
+        return jsonify({"status": "error", "message": "feature is not available for this video"}), 500
+    
+    
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
