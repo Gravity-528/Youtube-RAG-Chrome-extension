0a97f82896b3922e85fcd7ebad7ea12ff693252d
Gravity-528 <btech10086.22@bitmesra.ac.in>
2025-05-29 18:16:18 +0530

    webpack

---
+++ extension/src/content.js
@@ -0,0 +1,12 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "videoId") {
+        // console.log("Content script received videoId:", request.videoId);
+        // console.log("Sender:", sender);
+        // console.log("Request:", request);
+        // console.log("Sender tab ID:", sender.tab ? sender.tab.id : "No tab info");
+        
+        sendResponse({ status: "success", videoId: request.videoId });
+    } else {
+        sendResponse({ status: "error", message: "Unknown action" });
+    }
+})
---
+++ extension/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    popup: './src/popup.js',
+    background: './src/background.js',
+    content: './src/content.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/popup.html',
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' }
+      ]
+    })
+  ]
+};
