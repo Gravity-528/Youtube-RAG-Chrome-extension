1c98273fff1a276b6979e7b3b13f4cce95a08cc4
Gravity-528 <btech10086.22@bitmesra.ac.in>
2025-07-22 09:09:30 +0530

    sync it

--- server/main.py
+++ server/main.py
@@ -3,7 +3,8 @@ from app import app
 import os
 
 def main():
-    uvicorn.run(app,port=8000,host="0.0.0.0")
+    port = int(os.getenv("PORT", 8000))  
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
