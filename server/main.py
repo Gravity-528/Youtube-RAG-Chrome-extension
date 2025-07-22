3cc1e2d0d3ac7671dadf959a60c3a412f427a33e
Gravity-528 <btech10086.22@bitmesra.ac.in>
2025-07-22 11:33:15 +0530

    sync it

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
-import uvicorn
+# import uvicorn
+# from app import app
+# import os
+
+# def main():
+#     port = int(os.getenv("PORT", 8000))  
+#     uvicorn.run(app, host="0.0.0.0", port=port)
+
+# # if __name__ == "__main__":
+# #     main()
+# main()
+
 from app import app
-import os
-
-def main():
-    port = int(os.getenv("PORT", 8000))  
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    main()
